a0452de5daea613f45f231e6f6c662f03595c00e
Anwar5533 <anwarkornipalli@gmail.com>
2024-10-28 12:41:21 +0530

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+let role:[number,string];
+role = [5,"Anwar"]
+console.log(role)
+
+function App() {
+  return (
+      <div>
+          <h1 className="font-bold text-2xl">Hello world!</h1>
+      </div>
+  );
+}
+
+export default App
